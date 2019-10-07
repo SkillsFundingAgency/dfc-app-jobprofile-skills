cc3c5fe405bb69555e1593b50ea3385ffba16c27
kaderhashmi <52653766+kaderhashmi@users.noreply.github.com>
2019-10-07 11:52:52 +0100

    Create dummy function app for build pipeline

---
+++ DFC.App.JobProfilesSkills.MessageFunctionApp/Startup/WebJobsExtensionStartup.cs
@@ -0,0 +1,32 @@
+﻿using DFC.App.JobProfilesSkills.MessageFunctionApp.Models;
+using DFC.Functions.DI.Standard;
+using Microsoft.Azure.WebJobs;
+using Microsoft.Azure.WebJobs.Hosting;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+using System.Net.Http;
+
+[assembly: WebJobsStartup(typeof(DFC.App.JobProfilesSkills.MessageFunctionApp.Startup.WebJobsExtensionStartup), "Web Jobs Extension Startup")]
+
+namespace DFC.App.JobProfilesSkills.MessageFunctionApp.Startup
+{
+    public class WebJobsExtensionStartup : IWebJobsStartup
+    {
+        public void Configure(IWebJobsBuilder builder)
+        {
+            var configuration = new ConfigurationBuilder()
+                .SetBasePath(Environment.CurrentDirectory)
+                .AddJsonFile("local.settings.json", optional: true, reloadOnChange: true)
+                .AddEnvironmentVariables()
+                .Build();
+
+            var segmentClientOptions = configuration.GetSection("JobProfileSkillsSegmentClientOptions").Get<SegmentClientOptions>();
+
+            builder.AddDependencyInjection();
+
+            builder.Services.AddSingleton<SegmentClientOptions>(segmentClientOptions);
+            builder.Services.AddSingleton<HttpClient>(new HttpClient());
+        }
+    }
+}
