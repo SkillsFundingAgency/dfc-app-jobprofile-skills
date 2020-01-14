f950f4f3d91e4dbc7d268a1a1d246d24eb4c66b9
kaderhashmi <52653766+kaderhashmi@users.noreply.github.com>
2020-01-14 09:36:59 +0000

    Make Health Ping the default action (#51)

--- DFC.App.JobProfileSkills/Startup.cs
+++ DFC.App.JobProfileSkills/Startup.cs
@@ -90,7 +90,11 @@ namespace DFC.App.JobProfileSkills
             app.UseHttpsRedirection();
             app.UseStaticFiles();
             app.UseCookiePolicy();
-            app.UseMvcWithDefaultRoute();
+
+            app.UseMvc(routes =>
+                routes.MapRoute(
+                    name: "default",
+                    template: "{controller=Health}/{action=Ping}"));
 
             mapper?.ConfigurationProvider.AssertConfigurationIsValid();
         }
