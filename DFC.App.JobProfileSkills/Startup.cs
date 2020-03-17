2ab45fddee1416f99a30971a7d29a8a3a4e1cc0b
vallabha <vallabha24@gmail.com>
2020-03-17 10:49:34 +0000

    Added new line

--- DFC.App.JobProfileSkills/Startup.cs
+++ DFC.App.JobProfileSkills/Startup.cs
@@ -65,6 +65,7 @@ namespace DFC.App.JobProfileSkills
             app.UseStaticFiles();
             app.UseCookiePolicy();
             app.UseRouting();
+
             app.UseEndpoints(endpoints =>
             {
                 endpoints.MapRazorPages();
